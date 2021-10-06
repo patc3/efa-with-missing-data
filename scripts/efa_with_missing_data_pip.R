2bbbf651092e6fc5cbe62903b86e92e5480502a7
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-10-06 11:27:19 -0400

    Add scores for missing df in pip and density plot of factor scores for mis and full

--- scripts/efa_with_missing_data_pip.R
+++ scripts/efa_with_missing_data_pip.R
@@ -11,8 +11,12 @@ efa <- get_efa(df)
 scores <- efa |> get_factor_scores(df)
 
 df_mis <- df |> impose_missing()
+scores_mis <- efa |> get_factor_scores(df_mis)
 
-
+scores_all <- list(full=scores, mis=scores_mis) |> bind_rows(.id="Missing")
+ggplot(scores_all |> pivot_longer(F1:F4), aes(x=value, fill=Missing)) + 
+  geom_density(alpha=.4) + 
+  facet_wrap(~name)
 
 
 
