6a244549e8d584dae286f50293b3b8110eadd72e
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-10-06 10:31:41 -0400

    Add imports, simulate data, get efa, and get factor scores to fn and pip

--- scripts/efa_with_missing_data_fn.R
+++ scripts/efa_with_missing_data_fn.R
@@ -5,3 +5,52 @@ EFA with missing data
 fn
 "
 
+#### imports ####
+library(tidyverse)
+library(psych)
+library(MASS)
+
+
+#### simulate data ####
+simulate_data <- function(n)
+{
+  r <- psych::sim()$model
+  df <- mvrnorm(n=n, mu=rep(0, ncol(r)), Sigma=r) |> as.data.frame()
+  colnames(df) <- colnames(r)
+  return(df)
+}
+
+
+
+#### efa ####
+get_efa <- function(df)
+{
+  efa <- fa(
+    r = df,
+    nfactors = 4,
+    rotate = "oblimin",
+    scores = "tenBerge",
+    fm = "minres",
+    oblique.scores = TRUE,
+    use = "pairwise",
+    cor = "cor",
+    missing = FALSE, impute = "median"
+  )
+  return(efa)
+}
+
+
+# add scores
+get_factor_scores <- function(df, efa)
+{
+  #scores <- efa$scores # matrix
+  scores <- predict(efa, df) |> as.data.frame() # predict.psych
+  v_factors <- paste0("F", 1:ncol(scores))
+  colnames(scores) <- v_factors
+  
+  # out
+  return(scores)
+}
+
+
+
--- scripts/efa_with_missing_data_pip.R
+++ scripts/efa_with_missing_data_pip.R
@@ -5,3 +5,23 @@ EFA with missing data
 pip
 "
 
+source("scripts/efa_with_missing_data_fn.R")
+df <- simulate_data(n=100000)
+efa <- get_efa(df)
+scores <- df |> get_factor_scores(efa)
+
+
+"
+goal is not estimate loadings, but get factor scores
+  so need fa for each md pattern to be able to predict scores with appropriate loadings
+  compare predict with all items efa and imputation (eg mice)
+  --> how to estimate bias? what is the outcome?
+
+one fa per md pattern
+use all obs each time (borrow information)
+cfa: to specify items load on which factor
+
+do regression as in satis and bias in reg coefs and hyp tests
+
+
+"
